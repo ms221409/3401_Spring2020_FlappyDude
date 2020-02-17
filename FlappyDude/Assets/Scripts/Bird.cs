ad86bc4121c185186d353b2f72759be66131cb8d
Michael Stephens <mstephens.dev@gmail.com>
2020-02-17 17:51:48 -0500

    Initial commit

---
+++ FlappyDude/Assets/Scripts/Bird.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Bird : MonoBehaviour
+{
+    public Rigidbody2D birdRigidbody;
+    public float flapForce;
+    public GameObject gameoverUI;
+
+    int score = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("flap is working");
+
+            //birdRigidbody.AddForce(Vector2.up * flapForce);
+            birdRigidbody.velocity = Vector2.up * flapForce;
+        }
+    }
+
+
+    //
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ScoreTag")
+        {
+            score += 1;
+            Debug.Log(score);
+        }
+        else
+        {
+            gameoverUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+
+    public void OnRestartButtonPressed ()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ FlappyDude/Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject pipeSetPrefab;
+    public float pipeSpawnTime;
+    public Transform spawnPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnPipeSet", 0, pipeSpawnTime);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void SpawnPipeSet ()
+    {
+        Instantiate(pipeSetPrefab, spawnPosition.position, Quaternion.identity);
+    }
+}
